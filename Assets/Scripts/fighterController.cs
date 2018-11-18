de0e95407c80b9840ed53476766cd15450542793
Shawn Oakley <shawnoakley@gmail.com>
2018-11-18 00:47:08 -0500

    Adds walking interaction and animations

---
+++ Assets/Scripts/fighterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fighterController : MonoBehaviour {
+    public Transform enemyTarget;
+    public Animator anim;
+    public static bool moveBack = false;
+    public static bool moveForward = false;
+    public static fighterController instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moveBack == true)
+        {
+            anim.SetTrigger("walkBack");
+            anim.ResetTrigger("idle");
+        }
+        else
+        {
+            anim.SetTrigger("idle");
+            anim.ResetTrigger("walkBack");
+        }
+
+        if(moveForward == true)
+        {
+            anim.SetTrigger("walkForward");
+            anim.ResetTrigger("idle");
+        }
+        else if (moveBack == false)
+        {
+            anim.SetTrigger("idle");
+            anim.ResetTrigger("walkForward");
+        }
+	}
+}
---
+++ Assets/Scripts/walkForwardController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class walkForwardController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        fighterController.moveForward = true;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        fighterController.moveForward = false;
+    }
+
+}
